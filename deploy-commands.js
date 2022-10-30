8de5d8844fcb32de172cdb861aa0323c431a8c2b
Venom <tomptaylor92@googlemail.com>
2022-10-30 23:40:16 +0000

    .env changes

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
+require('dotenv').config();
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 const fs = require('node:fs');
 const path = require('node:path');
+require('dotenv').config();
+
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 
